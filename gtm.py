8184c6d6dd67b620cae0ef240fd24aaffff62393
ValluvarSploit <94351646+the-valluvarsploit@users.noreply.github.com>
2023-09-24 10:53:40 +0530

    timeout support added
    Added timeout flag to avoid waiting.

--- gtm.py
+++ gtm.py
@@ -8,21 +8,26 @@ green = "\x1b[32m"
 red = "\x1b[31m"
 reset = "\x1b[0m"
 
+GTM_URL = "https://googletagmanager.com/gtm.js"
+
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--url", dest="url", help="Target URL")
     parser.add_argument("-g", "--gtm_id", dest="gtm_id", help="Google Tag Manager ID")
+    parser.add_argument("-t", "--timeout", dest="timeout", type=int, default=10, help="Timeout in seconds (default is 10)")
     args = parser.parse_args()
     if not (args.url or args.gtm_id):
         parser.print_help()
         exit()
     return args
 
-
-def fetch_gtm_id(url):
+def fetch_gtm_id(url, timeOut):
     try:
-        request = requests.get(url, verify=False)
+        request = requests.get(url, timeout=timeOut, verify=False)
         response = request.text
+    except requests.exceptions.Timeout:
+        print(f"{red}[-] Timeout occurred while fetching {url}{reset}")
+        exit()
     except Exception as e:
         print(f"{red}[-] Connection Error on {url}{reset}")
         exit()
@@ -37,14 +42,15 @@ def fetch_gtm_id(url):
     else:
         print(f"{red}[-] GTM ID is not found {url}{reset}")
 
-
-def fetch_subdomains(url, gtmId):
+def fetch_subdomains(url, gtmId, timeout):
     subdomains = []
     try:
-        request = requests.get(f"https://googletagmanager.com/gtm.js?id={gtmId}", allow_redirects=False)
+        request = requests.get(GTM_URL , params={"id": gtmId}, timeout=timeout, allow_redirects=False)
         if request.status_code != 200:
             print(f"{red}[-] Failed to fetch {url} {request.status_code}{reset}")
         response = request.text
+    except requests.exceptions.Timeout:
+        print(f"{red}[-] Timeout occurred while fetching {url}{reset}")
     except Exception as e:
         print(f"{red}[-] Failed to fetch url {url} {e}{reset}")
 
@@ -60,12 +66,10 @@ def fetch_subdomains(url, gtmId):
     
     return subdomains
 
-
 def parse_root_domain(target):
     domain_parts = target.split("/")[2].split(".")
     rootDomain = domain_parts[-2] + "." + domain_parts[-1]
     return rootDomain
-
 
 def remove_duplicates(domains):
     encountered = {}
@@ -78,24 +82,24 @@ def remove_duplicates(domains):
 
     return unique_domains
 
-
 def main():
     ARGS = get_arguments()
     url = ARGS.url
     gtm_id = ARGS.gtm_id
+    timeOut = ARGS.timeout
+
     if url and gtm_id:
-        temp_subdomains = fetch_subdomains(url, gtm_id)
+        temp_subdomains = fetch_subdomains(url, gtm_id, timeOut)
         subdomains = remove_duplicates(temp_subdomains)
         for subs in subdomains:
             print(f"{green}{subs}{reset}")
     else:
-        gtm_id = fetch_gtm_id(url)
+        gtm_id = fetch_gtm_id(url, timeOut)
         if gtm_id is not None:
-            temp_subdomains = fetch_subdomains(url, gtm_id)
+            temp_subdomains = fetch_subdomains(url, gtm_id, timeOut)
             subdomains = remove_duplicates(temp_subdomains)
             for subs in subdomains:
                 print(f"{green}{subs}{reset}")
-
 
 if __name__ == "__main__":
     main()
